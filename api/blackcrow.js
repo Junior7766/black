231019a2ead1ae877039176a01065b63af9b9212
Junior7766 <119082257+Junior7766@users.noreply.github.com>
2025-05-21 08:09:48 -0300

    Update blackcrow.js

--- api/blackcrow.js
+++ api/blackcrow.js
@@ -6,11 +6,36 @@ export default function handler(req, res) {
 
   if (codigosValidos.includes(code)) {
     const conteudo = `
-      Bem-vindo ao BlackCrowTv!
-      Lista de canais:
-      - Canal 1
-      - Canal 2
-      - Canal 3
+      {
+    "user_info": {
+        "username": "tydhws",
+        "password": "mv0ebe",
+        "message": "",
+        "auth": 1,
+        "status": "Active",
+        "exp_date": "2147483647",
+        "is_trial": "0",
+        "active_cons": "1",
+        "created_at": "1672332438",
+        "max_connections": "999",
+        "allowed_output_formats": [
+            "m3u8",
+            "ts",
+            "rtmp"
+        ]
+    },
+    "server_info": {
+        "url": "cdn.brpro.live",
+        "port": "80",
+        "https_port": "443",
+        "server_protocol": "http",
+        "rtmp_port": "8080",
+        "timezone": "America/Sao_Paulo",
+        "timestamp_now": 1709960182,
+        "time_now": "2024-03-09 01:56:22",
+        "process": true
+    }
+}
     `;
     res.status(200).send(conteudo);
   } else {
